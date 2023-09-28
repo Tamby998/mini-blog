d908e08967a78ad4425be9220b812760f52d2fc9
Tamby Mampionona <therihar@etceterum.net>
2023-09-28 12:06:56 +0300

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    // Functions index 
+    public function index() {
+        $posts =  Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    // Fuction create post
+    public function create() {
+        return view('posts.create');
+    }
+
+    // Fuction store 
+    public function store(Request $request) {
+        // Validate post
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $post =  new Post();
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('images'), $file_name);
+
+        $post->title =  $request->title;
+        $post->description = $request->description;
+        $post->image = $file_name;
+
+        $post->save();
+        return redirect()->route('posts.index')->with('success', 'Post has been successfully');
+    }
+}
